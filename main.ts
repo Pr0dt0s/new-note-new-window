e5b717fdb17f8bffdd5330aaa1ecfbb376f18710
Pr0dt0s <prodtos2@gmail.com>
2022-10-29 12:31:33 -0300

    code

--- main.ts
+++ main.ts
@@ -1,100 +1,110 @@
 import {
   App,
-  Editor,
-  MarkdownView,
-  Modal,
-  Notice,
   Plugin,
   PluginSettingTab,
   Setting,
+  WorkspaceLeaf,
 } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-  mySetting: string;
+interface LeafStatus {
+  leaf: WorkspaceLeaf;
+  status: 'new' | 'loaded';
+}
+interface NewNoteNewWindowSettings {
+  openInLastFloatingWindow: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-  mySetting: 'default',
+const DEFAULT_SETTINGS: NewNoteNewWindowSettings = {
+  openInLastFloatingWindow: true,
 };
 
-export default class MyPlugin extends Plugin {
-  settings: MyPluginSettings;
+export default class NewNoteNewWindow extends Plugin {
+  settings: NewNoteNewWindowSettings;
+
+  leaves: LeafStatus[] = [];
 
   async onload() {
     await this.loadSettings();
 
-    // This creates an icon in the left ribbon.
-    const ribbonIconEl = this.addRibbonIcon(
-      'dice',
-      'Sample Plugin',
-      (evt: MouseEvent) => {
-        // Called when the user clicks the icon.
-        new Notice('This is a notice!');
-      }
-    );
-    // Perform additional things with the ribbon
-    ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-    // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-    const statusBarItemEl = this.addStatusBarItem();
-    statusBarItemEl.setText('Status Bar Text');
-
-    // This adds a simple command that can be triggered anywhere
-    this.addCommand({
-      id: 'open-sample-modal-simple',
-      name: 'Open sample modal (simple)',
-      callback: () => {
-        new SampleModal(this.app).open();
-      },
-    });
-    // This adds an editor command that can perform some operation on the current editor instance
-    this.addCommand({
-      id: 'sample-editor-command',
-      name: 'Sample editor command',
-      editorCallback: (editor: Editor, view: MarkdownView) => {
-        console.log(editor.getSelection());
-        editor.replaceSelection('Sample Editor Command');
-      },
-    });
-    // This adds a complex command that can check whether the current state of the app allows execution of the command
-    this.addCommand({
-      id: 'open-sample-modal-complex',
-      name: 'Open sample modal (complex)',
-      checkCallback: (checking: boolean) => {
-        // Conditions to check
-        const markdownView =
-          this.app.workspace.getActiveViewOfType(MarkdownView);
-        if (markdownView) {
-          // If checking is true, we're simply "checking" if the command can be run.
-          // If checking is false, then we want to actually perform the operation.
-          if (!checking) {
-            new SampleModal(this.app).open();
+    const trackClose = (leafStatus: LeafStatus) => {
+      this.registerInterval(
+        window.setInterval(() => {
+          if (
+            leafStatus.status == 'loaded' &&
+            leafStatus.leaf.getViewState().type == 'empty'
+          ) {
+            this.leaves = this.leaves.filter((l) => l.leaf != leafStatus.leaf);
+            leafStatus.leaf.detach();
           }
+        }, 10)
+      );
+    };
 
-          // This command will only show up in Command Palette when the check function returns true
-          return true;
+    const createLeaf = (where: 'new-window' | 'same-window') => {
+      let leaf: WorkspaceLeaf | undefined = undefined;
+
+      if (!this.leaves.length) {
+        leaf = this.app.workspace.getLeaf('window');
+      } else {
+        if (where == 'same-window') {
+          let lastLeafStatus = this.leaves[this.leaves.length - 1];
+          this.app.workspace.setActiveLeaf(lastLeafStatus.leaf);
+          leaf = this.app.workspace.getLeaf('tab');
+        } else {
+          leaf = this.app.workspace.getLeaf('window');
         }
+      }
+      const newLeafStatus: LeafStatus = { leaf, status: 'new' };
+
+      this.app.workspace.on('active-leaf-change', () => console.log);
+
+      trackClose(newLeafStatus);
+      this.leaves.push(newLeafStatus);
+      return newLeafStatus;
+    };
+
+    this.addCommand({
+      id: 'nnnw-create-new-note-in-window',
+      name: 'Create note in Window',
+      icon: 'popup-open',
+      callback: async () => {
+        let fileName = this.generateNewFileNameInFolder();
+
+        let nleafStatus = this.settings.openInLastFloatingWindow
+          ? createLeaf('same-window')
+          : createLeaf('new-window');
+        const newFile = await this.app.vault.create(fileName, '', {});
+        await nleafStatus.leaf.openFile(newFile);
+        nleafStatus.status = 'loaded';
+        console.log(this.leaves);
       },
     });
 
-    // This adds a settings tab so the user can configure various aspects of the plugin
-    this.addSettingTab(new SampleSettingTab(this.app, this));
-
-    // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-    // Using this function will automatically remove the event listener when this plugin is disabled.
-    this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-      console.log('click', evt);
-    });
-
-    // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-    this.registerInterval(
-      window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000)
-    );
+    this.addSettingTab(new SettingTab(this.app, this));
   }
 
-  onunload() {}
+  private generateNewFileNameInFolder() {
+    let lastFile = this.app.workspace.getActiveFile();
+    let path = !!lastFile ? lastFile.path : '';
+    const tfolder = this.app.fileManager.getNewFileParent(path);
+    let newFilePath = tfolder.path;
+    let untitleds = tfolder.children
+      .filter((c) => c.name.startsWith('Untitled'))
+      .map((c) => c.name);
+
+    let fileName = '';
+    for (let i = 0; i <= untitleds.length; i++) {
+      fileName = `Untitled${i > 0 ? ' ' + (i + 1) : ''}.md`;
+      if (!untitleds.includes(fileName)) {
+        break;
+      }
+    }
+    return `${newFilePath}/${fileName}`;
+  }
+
+  onunload() {
+    this.leaves = [];
+  }
 
   async loadSettings() {
     this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -105,49 +115,31 @@ export default class MyPlugin extends Plugin {
   }
 }
 
-class SampleModal extends Modal {
-  constructor(app: App) {
-    super(app);
-  }
+class SettingTab extends PluginSettingTab {
+  plugin: NewNoteNewWindow;
 
-  onOpen() {
-    const { contentEl } = this;
-    contentEl.setText('Woah!');
-  }
-
-  onClose() {
-    const { contentEl } = this;
-    contentEl.empty();
-  }
-}
-
-class SampleSettingTab extends PluginSettingTab {
-  plugin: MyPlugin;
-
-  constructor(app: App, plugin: MyPlugin) {
+  constructor(app: App, plugin: NewNoteNewWindow) {
     super(app, plugin);
     this.plugin = plugin;
   }
 
-  display(): void {
+  display() {
     const { containerEl } = this;
 
     containerEl.empty();
-
-    containerEl.createEl('h2', { text: 'Settings for my awesome plugin.' });
+    containerEl.createEl('h2', {
+      text: `${this.plugin.manifest.name} ${this.plugin.manifest.version}`,
+    });
 
     new Setting(containerEl)
-      .setName('Setting #1')
-      .setDesc("It's a secret")
-      .addText((text) =>
-        text
-          .setPlaceholder('Enter your secret')
-          .setValue(this.plugin.settings.mySetting)
-          .onChange(async (value) => {
-            console.log('Secret: ' + value);
-            this.plugin.settings.mySetting = value;
-            await this.plugin.saveSettings();
-          })
-      );
+      .setName('Reuse Floating Window')
+      .setDesc('If true new notes will open in the same floating window.')
+      .addToggle((cb) => {
+        cb.setValue(this.plugin.settings.openInLastFloatingWindow);
+        cb.onChange(async (value) => {
+          this.plugin.settings.openInLastFloatingWindow = value;
+          await this.plugin.saveSettings();
+        });
+      });
   }
 }
