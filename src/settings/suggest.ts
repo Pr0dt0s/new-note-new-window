7024eeb82c893c4b8ae4854cb104ea53eea00ac0
Pedro Reyes <prodtos2@gmail.com>
2023-04-17 19:15:30 -0300

    fix dom ts error

--- src/settings/suggest.ts
+++ src/settings/suggest.ts
@@ -143,6 +143,7 @@ export abstract class TextInputSuggest<T> implements ISuggestOwner<T> {
 
     if (suggestions.length > 0) {
       this.suggest.setSuggestions(suggestions);
+      //@ts-ignore
       this.open(app.dom.appContainerEl, this.inputEl);
     } else {
       this.close();
